e5041881d9d615a72b3252033e1e48d7689666fb
Shay <shayenemeiraf@gmail.com>
2025-08-26 12:59:13 -0300

    Create operadores_moeda.py

---
+++ operadores_moeda.py
@@ -0,0 +1,6 @@
+pesos = int(input('Quantos pesos você tem?'))
+soles = int(input('Quantos soles você tem?'))
+reais = int(input('Quantos reais você tem?'))
+
+total = pesos * 0.00025 + soles * 0.28 + reais * 0.21
+print(total)
